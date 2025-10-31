363370fdacea8f088a28ce262c78c0646abd6e29
aiwithnick <nickwestburgai@gmail.com>
2025-10-30 22:53:34 -0400

    fix: disable publicDir copying to eliminate 3.7GB thumbnails from dist (SQL API migration)

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   root: 'src',
-  publicDir: '../public',
+  publicDir: false, // Disable automatic public directory copying - SQL API migration
   build: {
     outDir: '../dist',
     emptyOutDir: true,
